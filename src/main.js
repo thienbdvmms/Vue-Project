a2685e80940d59d47ebdc2554a47c05dd0a0716b
buiducthien <thienbd@vmms.com.vn>
2023-02-28 22:54:53 +0700

    feat: config router VueJS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createRouter, createWebHashHistory } from "vue-router";
+
+import HelloWorld from "./components/HelloWorld.vue";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/z",
+      component: HelloWorld,
+    },
+  ],
+});
+
+createApp(App).use(router).mount("#app");
+
+// new Vue({
+//     render: h => h(App),
+//     // router,
+// }).$mount('app')
